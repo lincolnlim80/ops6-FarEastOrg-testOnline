8bac4040f432b70e9df8a9cbe6c4142ffe7181e4
lincolnlim80 <mypropertyagent88@gmail.com>
2023-06-14 22:37:38 +0800

    testlaunch

---
+++ index.js
@@ -0,0 +1,17 @@
+function showGetInTouchSales() {
+    document.getElementById("popupFormSales").style.display = "block";
+  }
+
+function showGetInTouchGeneral() {
+    document.getElementById("popupFormGeneral").style.display = "block";
+  }
+
+function closeFormSales() {
+    document.getElementById("popupFormSales").style.display = "none";
+  }
+
+function closeFormGeneral() {
+  document.getElementById("popupFormGeneral").style.display = "none";
+}
+
+
